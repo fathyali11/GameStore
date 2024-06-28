f0ce82e8ab12b021773aa7e0aa1925d13bbe3df8
fathyali11 <fathy.ali8ali@gmail.com>
2024-06-28 12:25:55 +0300

    intital commit with change bootstrap and font of my website

---
+++ GameStoreProject/GameStoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameStoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
